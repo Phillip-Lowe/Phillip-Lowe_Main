4c832bad7ee3ad4d47d82753200ea0ec1dd136f0
Phillip Lowe <philliplowe@Phillips-MacBook-Air.local>
2026-06-23 04:30:45 -0500

    fix: Remove accidentally committed utopia-deli-temp directory

--- utopia-deli-temp/square-data/Customer Table.sql
+++
@@ -1,9 +0,0 @@
-Drop table If Exists Customer;
-CREATE TABLE Customer (
-    SquareCustomerID NVARCHAR(255) NOT NULL,
-    UtopiaID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    Name NVARCHAR(255) NOT NULL,
-    Email NVARCHAR(255),
-    Phone NVARCHAR(255),
-    Anonymous BIT NOT NULL DEFAULT 0
-);
